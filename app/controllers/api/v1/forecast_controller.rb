6de951fc999be7746af0bf766d68782c122e3caa
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-12 19:40:32 -0600

    Adds to show method.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ForecastController < ApplicationController
   def show
+    location = LocationService.new(look_up_params)
+    forecast = ForecastService.new(location.lat, location.long)
+    render json: ForecastSerializer.new(forecast)
+  end
 
+  private
+  def look_up_params
+    params['location']
   end
 end
