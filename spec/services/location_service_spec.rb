f08ad5ddb983fd0049bbe419eaca77c2dbc895f4
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-12 19:09:28 -0600

    Adds location service spec.

---
+++ spec/services/location_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+context 'Location Service' do
+  it 'exists' do
+    location = LocationService.new('denver,co')
+    expect(location).to be_a(Location)
+  end
+
+  it 'when given city it returns a latitude & longtitude' do
+    location = LocationService.new('denver,co')
+    expect(location).to eq("some lat & long")
+  end
+end
