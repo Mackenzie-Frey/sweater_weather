845356b3dd78d2899f56f87eff803c3e117464b7
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 09:03:42 -0600

    Alters expectations and setup.

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 context 'Location Service' do
   it 'exists' do
     location = LocationService.new('denver,co')
+
     expect(location).to be_a(LocationService)
   end
 
   it 'when given city it returns a latitude & longtitude' do
-    location = LocationService.new('denver,co')
-    expect(location).to eq("some lat & long")
+    lat = LocationService.new('denver,co').lat
+    long = LocationService.new('denver,co').long
+
+    expect(lat).to eq(39.7392358)
+    expect(long).to eq(-104.990251)
   end
 end
