64dc33dc972d9df8d2eae6b9fa3c1543c2d86f27
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-12 20:26:04 -0600

    Calls location method on service.

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    location = LocationService.new(look_up_params)
+    location = LocationService.new(look_up_params).location
     forecast = ForecastService.new(location.lat, location.long)
     render json: ForecastSerializer.new(forecast)
   end
