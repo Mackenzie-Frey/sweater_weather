59b8daa21161f56e4ff27b93331b357a91e995bd
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 09:29:24 -0600

    Interpolate @lat and @long.

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -5,12 +5,12 @@ class ForecastService
   end
 
   def forecast
-    conn.get("/forecast/#{ENV['dark_sky_key']}/@lat,@long")
+    conn.get("/forecast/#{ENV['dark_sky_key']}/#{@lat},#{@long}")
   end
 
   def conn
     Faraday.new(url: 'https://api.darksky.net/') do |faraday|
-
+      faraday.params['exclude'] = 'minutely'
       faraday.adapter Faraday.default_adapter
     end
   end
