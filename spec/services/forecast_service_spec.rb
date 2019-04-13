37640f5305cc739a8c36ac47f831f509346f118d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 09:56:59 -0600

    Adds expectations for response keys.

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -14,8 +14,34 @@ context 'Forescast Service' do
 
   it 'returns weather data when given a latitude and longitude' do
     forecast = ForecastService.new(@lat, @long).forecast
-binding.pry
+
     expect(forecast).to be_a(Hash)
 
+    expect(forecast[:currently]).to have_key(:time)
+    expect(forecast[:currently]).to have_key(:summary)
+    expect(forecast[:currently]).to have_key(:icon)
+    expect(forecast[:currently]).to have_key(:temperature)
+    expect(forecast[:currently]).to have_key(:humidity)
+    expect(forecast[:currently]).to have_key(:visibility)
+    expect(forecast[:currently]).to have_key(:uvIndex)
+    expect(forecast[:currently]).to have_key(:apparentTemperature)
+
+    expect(forecast[:hourly]).to have_key(:summary)
+    expect(forecast[:hourly][:data][0]).to have_key(:time)
+    expect(forecast[:hourly][:data][0]).to have_key(:summary)
+    expect(forecast[:hourly][:data][0]).to have_key(:icon)
+    expect(forecast[:hourly][:data][0]).to have_key(:precipProbability)
+    expect(forecast[:hourly][:data][0]).to have_key(:temperature)
+    expect(forecast[:hourly][:data][0]).to have_key(:apparentTemperature)
+    expect(forecast[:hourly][:data][0]).to have_key(:humidity)
+
+
+    expect(forecast[:daily]).to have_key(:summary)
+    expect(forecast[:daily][:data][0]).to have_key(:time)
+    expect(forecast[:daily][:data][0]).to have_key(:icon)
+    expect(forecast[:daily][:data][0]).to have_key(:precipProbability)
+    expect(forecast[:daily][:data][0]).to have_key(:precipType)
+    expect(forecast[:daily][:data][0]).to have_key(:temperatureHigh)
+    expect(forecast[:daily][:data][0]).to have_key(:temperatureLow)
   end
 end
