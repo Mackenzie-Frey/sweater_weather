5416c71ef7dad98ab9ff00bc0733fd517cdea402
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 16:48:49 -0600

    Alters expectations and test naming.

--- spec/services/background_service_spec.rb
+++ spec/services/background_service_spec.rb
@@ -9,10 +9,11 @@ context 'Background Service' do
     expect(@background).to be_a(BackgroundService)
   end
 
-  it 'outputs a background image url' do
+  it 'outputs a background image object' do
     photo = 'https://images.unsplash.com/photo-1546156929-a4c0ac411f47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjY2NTE1fQ'
+    background_data = @background.images
 
-    expect(@background.url).to be_a(String)
-    expect(@background.url).to eq(photo)
+    expect(background_data).to be_a(Unsplash::Photo)
+    expect(background_data[:urls][:raw]).to eq(photo)
   end
 end
