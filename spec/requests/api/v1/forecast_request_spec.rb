4be2393410815316ff158e75491d6c42325b734e
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 11:12:03 -0600

    Adds test expectations for JSON output.

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -5,11 +5,35 @@ context '/api/v1/forecast?location=denver,co' do
 
     get '/api/v1/forecast?location=denver,co'
 
-    expected_data = "thing"
-
-    result = JSON.parse(response.body)
+    result = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(result).to eq(expected_data)
+    
+    expect(result[:forecast][:currently]).to have_key(:time)
+    expect(result[:forecast][:currently]).to have_key(:summary)
+    expect(result[:forecast][:currently]).to have_key(:icon)
+    expect(result[:forecast][:currently]).to have_key(:temperature)
+    expect(result[:forecast][:currently]).to have_key(:humidity)
+    expect(result[:forecast][:currently]).to have_key(:visibility)
+    expect(result[:forecast][:currently]).to have_key(:uvIndex)
+    expect(result[:forecast][:currently]).to have_key(:apparentTemperature)
+
+    expect(result[:forecast][:hourly]).to have_key(:summary)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:time)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:summary)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:icon)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:precipProbability)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:temperature)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:apparentTemperature)
+    expect(result[:forecast][:hourly][:data][0]).to have_key(:humidity)
+
+
+    expect(result[:forecast][:daily]).to have_key(:summary)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:time)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:icon)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:precipProbability)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:precipType)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:temperatureHigh)
+    expect(result[:forecast][:daily][:data][0]).to have_key(:temperatureLow)
   end
 end
