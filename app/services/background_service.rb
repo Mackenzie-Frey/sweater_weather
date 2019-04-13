d1bfc745a1a9c23415e394ac2dff15cac800af7a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 16:25:27 -0600

    Adds methods to service.

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -3,6 +3,11 @@ class BackgroundService
     @location = location
   end
 
-  
+  def url
+    Unsplash::Photo.search(sanitize_location_params)[0][:urls][:raw]
+  end
 
+  def sanitize_location_params
+    @location.split(',')[0]
+  end
 end
