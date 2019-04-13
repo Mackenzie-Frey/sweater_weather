bcb159653a1d80974bcb0f16de0facac6c20263a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 16:41:28 -0600

    Adds implementation for creating a background PORO.

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::BackgroundController < ApplicationController
   def show
-    background_data = BackgroundService.new(look_up_params)
-    render json: BackgroundSerializer.new(background_data)
+    background_data = BackgroundService.new(look_up_params).url
+    render json: BackgroundSerializer.new(Background.new(background_data))
   end
 
   private
   def look_up_params
-    binding.pry
-    params['']
+    params['location']
   end
 end
