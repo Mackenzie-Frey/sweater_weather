51c0cbe898d9f2f39317457017127cc38b58a37b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 09:22:27 -0600

    Adds forecast and connection methods.

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -3,4 +3,15 @@ class ForecastService
     @lat = lat
     @long = long
   end
+
+  def forecast
+    conn.get("/forecast/#{ENV['dark_sky_key']}/@lat,@long")
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.darksky.net/') do |faraday|
+
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
