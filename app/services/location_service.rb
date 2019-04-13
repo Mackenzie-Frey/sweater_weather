8174511564ed5819a0cc0f46bd7553cc5d098748
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-12 20:26:46 -0600

    Adds service.

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -3,5 +3,27 @@ class LocationService
     @location = location
   end
 
-  
+  def lat
+  end
+
+  def long
+  end
+
+  def location
+    response = conn.get('/maps/api/geocode/json')
+    parse(response)
+  end
+
+  def parse
+    binding.pry
+
+  end
+
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com') do |faraday|
+      faraday.params[:key] = ENV['google_maps_key']
+      faraday.params[:address] = @location
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
 end
