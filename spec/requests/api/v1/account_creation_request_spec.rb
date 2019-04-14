24d917e68dcacf5872ff0f1e86763bdc2f722323
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 11:59:44 -0600

    Adds sad path testing for passwords not matching.

--- spec/requests/api/v1/account_creation_request_spec.rb
+++ spec/requests/api/v1/account_creation_request_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe 'Users API' do
-  it 'recieves user info and returns an api key' do
-
+  it 'recieves user info, creates a user, and returns an api key' do
     visitor_info = { "email": "whatever@example.com",
     "password": "password", "password_confirmation": "password" }
 
@@ -10,8 +9,20 @@ describe 'Users API' do
 
     result = JSON.parse(response.body)
 
-    expect(response).to have_status(200)
+    expect(response.code).to eq('200')
     expect(result).to be_a(Hash)
-    expect(result['api_key']).to eq('jgn983hy48thw9begh98h4539h4')
+    expect(result['data']['attributes']['api_key']).to be_a(String)
+    expect(result['data']['attributes']['api_key'].length).to eq(24)
+  end
+
+  it 'receives user info & does not create a user if passwords do not match' do
+    visitor_info = { "email": "whatever@example.com",
+    "password": "password", "password_confirmation": "password-doesn't-match" }
+
+    post '/api/v1/users', params: visitor_info
+binding.pry
+    expect(response.code).to eq('400')
   end
 end
+
+# sad path for passwords not matching
