44614cbb5b3a5c934f1eca65412d058934a07548
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 10:29:38 -0600

    Adds to create method in users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create!(params['email'], params['password'], params['password_confirmation'])
-    response = UserService.new(user)
-    render json: UserSerializer.new(response)
+    api_key = SecureRandom.hex(12)
+
+    user = User.new(email: input_params['email'],
+                    password: input_params['password'],
+                    password_confirmation: input_params['password_confirmation'],
+                    api_key: api_key)
+
+    if user.save
+      # response = UserService.new(user)
+      render json: UserSerializer.new(response)
+    else
+      render json: {status: 400, msg: 'Invalid Parameters'}
+    end
+  end
+
+  private
+  def input_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
