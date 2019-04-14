06f5d61f7cdf1c8251605a95f9898c85b10dfffd
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 19:36:48 -0600

    Adds account creation request spec.

---
+++ spec/requests/api/v1/account_creation_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Users API' do
+  it 'recieves user info and returns an api key' do
+
+    { "email": "whatever@example.com",
+    "password": "password", "password_confirmation": "password" }
+
+    post '/api/v1/users'
+
+    result = JSON.parse(response.body)
+
+    expect(response).to have_status(200)
+    expect(result).to be_a(Hash)
+    expect(result['api_key']).to eq('jgn983hy48thw9begh98h4539h4')
+  end
+end
