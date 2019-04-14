cfcdc314e8876817152440399bd5e61ea7935a78
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 15:57:14 -0600

    Adds favorite city relationship to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :favorite_cities
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password_digest, :api_key
   has_secure_password
