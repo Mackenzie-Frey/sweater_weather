f130b49bb7b3b18df71e5eb31ea6114ab83bdd5f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 19:43:16 -0600

    Changes backgrounds controller to be plural.

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::BackgroundController < ApplicationController
+class Api::V1::BackgroundsController < ApplicationController
   def show
     background_data = BackgroundService.new(look_up_params).images
     render json: BackgroundSerializer.new(Background.new(background_data))
