af2bde1bbb8b8fff3cf084ee7d51aa17403f5656
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 15:55:16 -0600

    Adds expectations and setup to request spec.

--- spec/requests/api/v1/favorite_location_request_spec.rb
+++ spec/requests/api/v1/favorite_location_request_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
-desribe 'Favorite Location API' do
+describe 'Favorite Location API' do
   before :each do
-    @email = 'email'
+    @email = 'email@email.com'
     @password = 'password123'
     @api_key = "jgn983hy48thw9begh98h4539h4"
+    @city = "Denver, CO"
 
-    User.create!(email: @email, password: @password, password_confirmation: @password, api_key: @api_key)
+    @user1 = User.create!(email: @email, password: @password, password_confirmation: @password, api_key: @api_key)
+    @user2 = User.create!(email: @email, password: @password, password_confirmation: @password, api_key: 'different-key')
   end
 
   it "receives a location & API key, then saves the location as the user's favorite" do
-    favorite_params = {"location": "Denver, CO",
+    favorite_params = {"location": @city,
                         "api_key": @api_key}
 
     post '/api/v1/favorites', params: favorite_params
 
-    expect(response.status).to eq(200)
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(204)
+    expect(@user1.favorite_cities[0].city).to eq(@city)
+    expect(@user2.favorite_cities[0].city).to_not eq(@city)
   end
 
   it "receives a location & an incorrect API key, and does not save the location as a user's favorite" do
-    favorite_params = {"location": "Denver, CO",
+    favorite_params = {"location": @city,
                         "api_key": "incorrect-key"}
 
     post '/api/v1/favorites', params: favorite_params
@@ -29,10 +32,15 @@ desribe 'Favorite Location API' do
   end
 
   it "receives a location, but no API key, and does not save the location as a user's favorite" do
-    favorite_params = {"location": "Denver, CO"}
+    favorite_params = {"location": @city}
 
     post '/api/v1/favorites', params: favorite_params
 
     expect(response.status).to eq(401)
   end
 end
+
+
+# test seems to be failing and may need to integrate database cleaner
+# Why is it giving a 401 unauthorized in Postman
+# add to favorites controller ?  skip_before_action :verify_authenticity_token
