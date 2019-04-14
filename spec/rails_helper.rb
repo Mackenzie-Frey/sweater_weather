fc73fc4caa4bcae2eb30971474e2ef1a987483f5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 10:29:24 -0600

    Adds shoulda-matchers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,6 +59,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Unsplash.configure do |config|
 #   config.application_access_key = ENV['unsplash_client_id']
