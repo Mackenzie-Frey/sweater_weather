0552dfc37d31675b80f5a83dc0d3ed3d8be3b905
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 12:49:06 -0600

    Alters to validates_presence_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
-  validates :password, :api_key, presence: true
+  validates_presence_of :password_digest, :api_key
   has_secure_password
 end
