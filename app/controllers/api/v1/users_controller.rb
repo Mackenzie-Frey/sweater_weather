0b74dc0fbd9ba3d638b9b7570909a6869a4c55aa
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 20:17:27 -0600

    Adds to users controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry
+    user = User.create!(params['email'], params['password'], params['password_confirmation'])
+    response = UserService.new(user)
+    render json: UserSerializer.new(response)
   end
 end
