5e30a97ffb1e6f70fdec8939c7d27869af5af3b9
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 18:04:00 -0600

    Comments out UnSplash setup.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,12 +60,12 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Unsplash.configure do |config|
-  config.application_access_key = ENV['unsplash_client_id']
-  config.application_secret = ENV['unsplash_client_secret']
-  config.application_redirect_uri = "https://your-application.com/oauth/callback"
-  config.utm_source = "alices_terrific_client_app"
-
-  # # optional:
-  # config.logger = MyCustomLogger.new
-end
+# Unsplash.configure do |config|
+#   config.application_access_key = ENV['unsplash_client_id']
+#   config.application_secret = ENV['unsplash_client_secret']
+#   config.application_redirect_uri = "https://your-application.com/oauth/callback"
+#   config.utm_source = "sweater_weather"
+#
+#   # # optional:
+#   # config.logger = MyCustomLogger.new
+# end
