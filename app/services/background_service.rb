eb6eba775ed56612a1b9d08d69a7d298145f8616
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-13 18:03:27 -0600

    Alters background service to not utilize UnSplash gem.

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -4,7 +4,21 @@ class BackgroundService
   end
 
   def images
-    Unsplash::Photo.search(sanitize_location_params)[0]
+    # Unsplash::Photo.search(sanitize_location_params)[0]
+    response = conn.get('/search/photos')
+    parse(response)
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.unsplash.com') do |faraday|
+      faraday.params['client_id'] = ENV['unsplash_client_id']
+      faraday.params['query'] = sanitize_location_params
+      faraday.adapter Faraday.default_adapter
+    end
   end
 
   def sanitize_location_params
