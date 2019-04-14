b006d9c896875b5e6c514eede7956c669486713c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-14 12:00:26 -0600

    Exctracts render_json to a private method.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,25 @@
 class Api::V1::UsersController < ApplicationController
   def create
     api_key = SecureRandom.hex(12)
-
+    #Make an api key object in initalize make key with attr_reader
     user = User.new(email: input_params['email'],
                     password: input_params['password'],
                     password_confirmation: input_params['password_confirmation'],
                     api_key: api_key)
 
-    if user.save
-      # response = UserService.new(user)
-      render json: UserSerializer.new(response)
-    else
-      render json: {status: 400, msg: 'Invalid Parameters'}
-    end
+    render_json(user)
   end
 
   private
   def input_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def render_json(user)
+    if user.save
+      render json: UserSerializer.new(user), status: 200
+    else
+      render json: {status: 400, msg: 'Invalid Parameters'}
+    end
   end
 end
