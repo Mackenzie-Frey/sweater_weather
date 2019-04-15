c7a4e95bcf2eeb7ee591d5a859c1cfa5ed3992ca
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 10:11:29 -0600

    Adds to amypode service and expectations to amypode service spec. Passes all amypode service tests.

--- app/services/amypode_service.rb
+++ app/services/amypode_service.rb
@@ -6,7 +6,22 @@ class AmypodeService
   end
 
   def antipode
-    conn.get('')
-    parse(reponse)
+    response = conn.get("/api/v1/antipodes?lat=#{@lat}&long=#{@long}")
+    parse(response)
+  end
+
+  def antipode_lat
+    antipode[:data][:attributes][:lat]
+  end
+
+  def antipode_long
+    antipode[:data][:attributes][:long]
+  end
+
+  def conn
+    Faraday.new(url: 'http://amypode.herokuapp.com') do |faraday|
+      faraday.headers['api_key'] = ENV['amypode_key']
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
--- spec/services/amypode_service_spec.rb
+++ spec/services/amypode_service_spec.rb
@@ -12,9 +12,20 @@ describe AmypodeService do
     expect(amypode_service).to be_a(AmypodeService)
   end
 
+
   it '#antipode -> receives lat and long and returns the antipode' do
+    response = {:data=>{:id=>'1', :type=>'antipode', :attributes=>{:lat=>-27.0, :long=>98.0}}}
+
     antipode = AmypodeService.new(@lat, @long).antipode
 
-    expect(antipode).to eq('Some Antipode City Name')
+    expect(antipode).to eq(response)
+  end
+
+  it '#antipode -> receives lat and long and returns the antipode lat & long' do
+    lat = AmypodeService.new(@lat, @long).antipode_lat
+    long = AmypodeService.new(@lat, @long).antipode_long
+
+    expect(lat).to eq(-27.0)
+    expect(long).to eq(98.0)
   end
 end
