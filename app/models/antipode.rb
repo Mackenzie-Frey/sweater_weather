cb34f1e5766cfb871e151faa449c0bace031e8f4
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 11:08:13 -0600

    Adds antipode model.

---
+++ app/models/antipode.rb
@@ -0,0 +1,10 @@
+class Antipode
+  def initialize(look_up_city, antipode_city_name, antipode_forecast)
+    @look_up_city = look_up_city
+    @antipode_city_name = antipode_city_name
+    @antipode_city_name = antipode_city_name
+    binding.pry
+  end
+
+
+end
