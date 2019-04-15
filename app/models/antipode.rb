0ecb87a0e3ee751fe1edeff5e97bf81f2ae188da
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 11:13:40 -0600

    Adds antipode methods and integartes with antipode serializer.

--- app/models/antipode.rb
+++ app/models/antipode.rb
@@ -2,9 +2,19 @@ class Antipode
   def initialize(look_up_city, antipode_city_name, antipode_forecast)
     @look_up_city = look_up_city
     @antipode_city_name = antipode_city_name
-    @antipode_city_name = antipode_city_name
-    binding.pry
+    @antipode_forecast = antipode_forecast
   end
 
+  def location_name
+    @antipode_city_name
+  end
 
+  def forecast
+    {summary: @antipode_forecast.current_summary,
+      current_temperature: @antipode_forecast.current_temperature}
+  end
+
+  def search_location
+    @look_up_city
+  end
 end
