0619a241ea1df7ff5517b3f06b47a4bc800aeb97
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 10:32:03 -0600

    Changes variable names from antipode_lat/long to lat/long.

--- spec/services/reverse_geocoding_service_spec.rb
+++ spec/services/reverse_geocoding_service_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 describe ReverseGeocodingService do
   before :each do
-    @antipode_lat = -22.3193039
-    @antipode_long = -65.8306389
+    @lat = -22.3193039
+    @long = -65.8306389
   end
 
   it 'exists' do
-    reverse_geocoding_service = ReverseGeocodingService.new(@antipode_lat, @antipode_long)
+    reverse_geocoding_service = ReverseGeocodingService.new(@lat, @long)
 
     expect(reverse_geocoding_service).to be_a(ReverseGeocodingService)
   end
 
   it '#city -> receives a lat and long and returns a city' do
-    city = ReverseGeocodingService.new(@antipode_lat, @antipode_long).city
+    city = ReverseGeocodingService.new(@lat, @long).city
 
     expect(city).to eq("Something like La Paz")
   end
