d45c1912acc812a410350e33fd09d1a1eb6727ef
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 09:54:42 -0600

    Adds Amypode Service Spec.

---
+++ spec/services/amypode_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe LocationService do
+  before :each do
+    @lat = 27
+    @long = -82
+  end
+
+  it 'exists' do
+    amypode_service = AmypodeService.new(@lat, @long)
+
+    expect(amypode_service).to be_a(AmypodeService)
+  end
+
+  it '#antipode -> receives lat and long and returns the antipode' do
+    antipode = AmypodeService.new(@lat, @long).antipode
+
+    expect(antipode).to eq('Some Antipode City Name')
+  end
+end
