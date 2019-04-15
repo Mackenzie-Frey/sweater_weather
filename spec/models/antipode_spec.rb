c1032bfe0d4448513557da2967b45bd1211aeaab
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 11:04:56 -0600

    Adds Antipode model spec.

---
+++ spec/models/antipode_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Antipode, type: :model do
+  antipode_lat = -22.3193039
+  antipode_long = -65.8306389
+
+  forecast_data = ForecastService.new(antipode_lat, antipode_long).forecast
+  @forecast = Forecast.new(forecast_data)
+  @look_up_city = "hongkong"
+  @antipode_city_name = "Jujuy, Argentina"
+
+  it 'exists' do
+    antipode = Antipode.new(@look_up_city, @antipode_city_name, @forecast)
+
+    expect(antipode).to be_a(Antipode)
+  end
+
+  it '' do
+
+  end
+end
