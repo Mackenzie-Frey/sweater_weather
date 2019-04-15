a2fb764662b62a66ef37624d99821f72111786e1
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 09:38:28 -0600

    Adds to antipode controller.

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::AntipodeController < ApplicationController
   def show
-    look_up_params['loc']
-    binding.pry
+    look_up_city = look_up_params['loc']
+    lat = ReverseGeocodingService.new(look_up_city).lat
+    long = ReverseGeocodingService.new(look_up_city).long
+
+    antipode = AmypodeService.new(lat, long).antipode
+
+    forecast_data = ForecastFacade.new(antipode).forecast
+
+    # render json: ForecastSerializer.new(Forecast.new(forecast_data))
+
+    render json: AntipodeSerializer.new(look_up_city, forecast_data)
   end
 
   private
