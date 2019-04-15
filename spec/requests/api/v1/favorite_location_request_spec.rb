70995ea768f7a8812097c78d9f6374810f3bce5b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 13:37:58 -0600

    Changes user2 email to a different email. Passes all tests.

--- spec/requests/api/v1/favorite_location_request_spec.rb
+++ spec/requests/api/v1/favorite_location_request_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 describe 'Favorite Location API' do
   before :each do
-    @email = 'email@email.com'
     @password = 'password123'
     @api_key = "jgn983hy48thw9begh98h4539h4"
     @city = "Denver, CO"
 
-    @user1 = User.create!(email: @email, password: @password, password_confirmation: @password, api_key: @api_key)
-    @user2 = User.create!(email: @email, password: @password, password_confirmation: @password, api_key: 'different-key')
+    @user1 = User.create!(email: 'email@email.com', password: @password, password_confirmation: @password, api_key: @api_key)
+    @user2 = User.create!(email: 'different-email', password: @password, password_confirmation: @password, api_key: 'different-key')
   end
 
-  xit "receives a location & API key, then saves the location as the user's favorite" do
+  it "receives a location & API key, then saves the location as the user's favorite" do
     favorite_params = {"location": @city,
                         "api_key": @api_key}
 
@@ -19,10 +18,10 @@ describe 'Favorite Location API' do
 
     expect(response.status).to eq(204)
     expect(@user1.favorite_cities[0].city).to eq(@city)
-    expect(@user2.favorite_cities[0].city).to_not eq(@city)
+    expect(@user2.favorite_cities).to eq([])
   end
 
-  xit "receives a location & an incorrect API key, and does not save the location as a user's favorite" do
+  it "receives a location & an incorrect API key, and does not save the location as a user's favorite" do
     favorite_params = {"location": @city,
                         "api_key": "incorrect-key"}
 
@@ -31,7 +30,7 @@ describe 'Favorite Location API' do
     expect(response.status).to eq(401)
   end
 
-  xit "receives a location, but no API key, and does not save the location as a user's favorite" do
+  it "receives a location, but no API key, and does not save the location as a user's favorite" do
     favorite_params = {"location": @city}
 
     post '/api/v1/favorites', params: favorite_params
@@ -40,6 +39,5 @@ describe 'Favorite Location API' do
   end
 end
 
-# skipped tests seems to be failing and may need to integrate database cleaner
 # Why is it giving a 401 unauthorized in Postman
 # add to favorites controller ?  skip_before_action :verify_authenticity_token
