80e843f33aad949493fb40f266097303403dd77b
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 22:01:47 -0600

    Adds to Favorites Controller.

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -9,11 +9,27 @@ class Api::V1::FavoritesController < ApiController
   end
 
   def index
-    User.find_by(api_key: json_body[:api_key]).favorite_cities
+    user = User.find_by(api_key: json_body[:api_key])
+    binding.pry
+    if user
+      fav_cities_forecasts = FavoriteCitiesFacade.new(extract_city_names(user)).forecasts
+      binding.pry
+      render json: FavoriteCitiesSerializer.new(fav_cities_forecasts)
+    else
+      render json: {}, status: 401
+    end
   end
 
   private
   def json_body
     JSON.parse(request.raw_post, symbolize_names: true)
+  end
+
+  def extract_city_names(user)
+    binding.pry
+    user.favorite_cities.map do |fav_city|
+      binding.pry
+      fav_city.city
+    end.uniq
   end
 end
