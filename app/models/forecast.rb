ddf259a910b620a944eaee5b5ae1667ba7ae6fa3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-15 22:18:56 -0600

    Deletes ForecastId module.

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,14 +1,13 @@
 class Forecast
   include CurrentWeather
-  include ForecastId
 
   def initialize(forecast_data)
     @forecast_data = forecast_data
   end
 
-  # def id
-  #   "#{current_time}/" + "#{@forecast_data.lat}/" + "#{@forecast_data.long}"
-  # end
+  def id
+    "#{current_time}/" + "#{lat}/" + "#{long}"
+  end
 
   def lat
     @forecast_data[:latitude]
@@ -21,38 +20,6 @@ class Forecast
   def currently
     @forecast_data[:currently]
   end
-
-  # def current_time
-  #   current_weather[:time]
-  # end
-  #
-  # def current_summary
-  #   current_weather[:summary]
-  # end
-  #
-  # def current_icon
-  #   current_weather[:icon]
-  # end
-  #
-  # def current_temperature
-  #   current_weather[:temperature]
-  # end
-  #
-  # def current_humidity
-  #   current_weather[:humidity]
-  # end
-  #
-  # def current_visibility
-  #   current_weather[:visibility]
-  # end
-  #
-  # def current_uv_index
-  #   current_weather[:uvIndex]
-  # end
-  #
-  # def current_apparant_temperature
-  #   current_weather[:apparentTemperature]
-  # end
 
   def hourly_summary
     @forecast_data[:hourly][:summary]
--- app/modules/forecast_id.rb
+++
@@ -1,5 +0,0 @@
-module ForecastId
-  def id
-    "#{current_time}/" + "#{@forecast_data.lat}/" + "#{@forecast_data.long}"
-  end
-end
