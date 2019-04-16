d30422d58fb645eaf54f80dcda47091ffd99c746
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-16 13:23:49 -0600

    Adds render_401 method.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,7 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
-  # skip_before_action :verify_authenticity_token
+
+  def render_401
+    render json: {}, status: 401
+  end
 end
