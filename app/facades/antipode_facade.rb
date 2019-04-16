fc4e36f0803b84c76a0c9e7949214f34c9f56e7a
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-16 15:43:55 -0600

    Integrates caching into Antipode Facade.

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -4,14 +4,20 @@ class AntipodeFacade
   end
 
   def antipode
-    coordinates = LocationService.new.location(@look_up_city)
+    coordinates = Rails.cache.fetch(@look_up_city) do
+        LocationService.new.location(@look_up_city)
+      end
 
-    antipode_coordinates = AmypodeService.new.antipode(coordinates)[:data][:attributes]
+    antipode_coordinates = Rails.cache.fetch(coordinates.to_s) do
+        AmypodeService.new.antipode(coordinates)[:data][:attributes]
+      end
 
     forecast_data = ForecastService.new(antipode_coordinates[:lat], antipode_coordinates[:long]).forecast
     antipode_forecast = Forecast.new(forecast_data)
 
-    antipode_city_name = LocationService.new.city(antipode_coordinates[:lat], antipode_coordinates[:long])
+    antipode_city_name = Rails.cache.fetch(antipode_coordinates[:lat].to_s + ',' + antipode_coordinates[:long].to_s) do
+        LocationService.new.city(antipode_coordinates[:lat], antipode_coordinates[:long])
+      end
 
     Antipode.new(@look_up_city, antipode_city_name, antipode_forecast)
   end
