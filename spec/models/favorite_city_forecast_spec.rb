c7efb400af5fff9a1196a7945c1eee9a85f1f416
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-16 09:27:42 -0600

    Adds to test expectations.

--- spec/models/favorite_city_forecast_spec.rb
+++ spec/models/favorite_city_forecast_spec.rb
@@ -30,20 +30,33 @@ describe FavoriteCityForecast, type: :model do
   end
 
   it '#currently' do
-    expect(@fav_city_forecast.currently).to be_a(Hash)
-    expect(@fav_city_forecast.currently).to have_key(:time)
-    expect(@fav_city_forecast.currently).to have_key(:summary)
-    expect(@fav_city_forecast.currently).to have_key(:icon)
-    expect(@fav_city_forecast.currently).to have_key(:precipProbability)
-    expect(@fav_city_forecast.currently).to have_key(:temperature)
-    expect(@fav_city_forecast.currently).to have_key(:apparentTemperature)
-    expect(@fav_city_forecast.currently).to have_key(:humidity)
-    expect(@fav_city_forecast.currently).to have_key(:uvIndex)
-    expect(@fav_city_forecast.currently).to have_key(:visibility)
+    currently = @fav_city_forecast.currently
+
+    expect(currently).to be_a(Hash)
+    expect(currently).to have_key(:time)
+    expect(currently).to have_key(:summary)
+    expect(currently).to have_key(:icon)
+    expect(currently).to have_key(:precipProbability)
+    expect(currently).to have_key(:temperature)
+    expect(currently).to have_key(:apparentTemperature)
+    expect(currently).to have_key(:humidity)
+    expect(currently).to have_key(:uvIndex)
+    expect(currently).to have_key(:visibility)
   end
 
-  it '#current_weather' do
-
-    expect(@fav_city_forecast.current_weather).to be_a(Hash)
+  xit '#current_weather' do
+    binding.pry
+    current_weather = @fav_city_forecast.current_weather
+    expect(current_weather).to be_a(Hash)
+    expect(current_weather.keys.count).to eq(8)
+    expect(current_weather).to have_key()
+    {:current_time=>1555428313,
+ :current_summary=>"Partly Cloudy",
+ :current_icon=>"partly-cloudy-day",
+ :current_temperature=>50.27,
+ :current_humidity=>0.61,
+ :current_visibility=>5.66,
+ :current_uv_index=>3,
+ :current_apparant_temperature=>50.27}
   end
 end
