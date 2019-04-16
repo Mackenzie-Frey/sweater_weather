119064679b7c8abd4fe6ecc07977749b0e706560
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-16 16:09:20 -0600

    Lowers caching to Location Service level, rather than the Facade.

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -4,9 +4,11 @@ class AntipodeFacade
   end
 
   def antipode
-    coordinates = Rails.cache.fetch(@look_up_city) do
-        LocationService.new.location(@look_up_city)
-      end
+    # coordinates = Rails.cache.fetch(@look_up_city) do
+    #     LocationService.new.location(@look_up_city)
+    #   end
+
+    coordinates = LocationService.new.location(@look_up_city)
 
     antipode_coordinates = Rails.cache.fetch(coordinates.to_s) do
         AmypodeService.new.antipode(coordinates)[:data][:attributes]
@@ -15,9 +17,10 @@ class AntipodeFacade
     forecast_data = ForecastService.new(antipode_coordinates[:lat], antipode_coordinates[:long]).forecast
     antipode_forecast = Forecast.new(forecast_data)
 
-    antipode_city_name = Rails.cache.fetch(antipode_coordinates[:lat].to_s + ',' + antipode_coordinates[:long].to_s) do
-        LocationService.new.city(antipode_coordinates[:lat], antipode_coordinates[:long])
-      end
+    # antipode_city_name = Rails.cache.fetch(antipode_coordinates[:lat].to_s + ',' + antipode_coordinates[:long].to_s) do
+    #     LocationService.new.city(antipode_coordinates[:lat], antipode_coordinates[:long])
+    #   end
+    antipode_city_name = LocationService.new.city(antipode_coordinates[:lat], antipode_coordinates[:long])
 
     Antipode.new(@look_up_city, antipode_city_name, antipode_forecast)
   end
--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -2,15 +2,19 @@ class LocationService
   include Service
 
   def location(location)
-    response = conn.get("/maps/api/geocode/json?address=#{location}")
-    coordinates = parse(response)[:results][0][:geometry][:location]
-    [ coordinates[:lat], coordinates[:lng] ]
+    Rails.cache.fetch(location + 'location_service') do
+      response = conn.get("/maps/api/geocode/json?address=#{location}")
+      coordinates = parse(response)[:results][0][:geometry][:location]
+      [ coordinates[:lat], coordinates[:lng] ]
+    end
   end
 
   def city(lat, long)
-    response = conn.get("/maps/api/geocode/json?latlng=#{location_params(lat,long)}")
-    result = parse(response)
-    parse_city(result)
+    Rails.cache.fetch(location_params(lat,long) + 'location_service') do
+      response = conn.get("/maps/api/geocode/json?latlng=#{location_params(lat,long)}")
+      result = parse(response)
+      parse_city(result)
+    end
   end
 
   def parse_city(result)
